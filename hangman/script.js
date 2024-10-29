05555eb95adf03b259e5988ace8a0f67d71f9c0e
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-10-29 09:50:04 +0100

    added files

---
+++ hangman/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Ord i "biblioteket" att välja på
+    
+    // Globala variabler
+
+    
+    // DOM referenser
+  
+    
+    // Initierar spelet
+  
+    
+    // Funktion för att uppdatera 
+   
+
+    // Funktion som körs när användaren skrivit in en bokstav
+   
+    // Funktion som kollor om man gissat rätt or
+  
+    
+    // Händelselyssnare på knappen
+    
+    // Starta spelet
+    
+    })
