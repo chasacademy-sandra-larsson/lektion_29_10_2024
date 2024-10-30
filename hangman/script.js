0d163e8acda98c616a64e657c7d1a61dd3e11c73
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-10-30 13:53:01 +0100

    added possible solution

--- hangman/script.js
+++ hangman/script.js
@@ -27,9 +27,9 @@ document.addEventListener("DOMContentLoaded", function() {
         // Noställ för att kunna resetta spelet
         guessLetters = [];
         remainingGuessess = 10;
-        gameMessageElement.textContent = "";
+        gameMessageElement.innerText = "";
         inputLetter.value = "";
-        wordElement.textContent = "";
+        wordElement.innerText = "";
     
         
         // Slumpa fram vilket ord i arrayen words
@@ -42,23 +42,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Hämta från input-rutan och för till gemener
         let letter = inputLetter.value.toLowerCase();
+        inputLetter.value = "";
 
 
         // Finns letter redan i guessLetters-arreyen? 
-        if(guessLetters.includes(letter) === true) {
-            gameMessageElement.textContent = "Du har redan gissat på bokstaven!!";
+        if(guessLetters.includes(letter) === true || letter === " ") {
+            gameMessageElement.innerText = "Du har redan gissat på bokstaven eller du gissade inte alls!!";
             return; // betyder att funktionen avslutas
         }
     
         // Lägga till letter sist i guessedLetter-arrayen
         guessLetters.push(letter);
-        console.log("guessLetters", guessLetters)
+     
 
         if(selectedWord.includes(letter) === true) {
-            gameMessageElement.textContent = "Rätt gissning!";
+            gameMessageElement.innerText = "Rätt gissning!";
         } else {
             remainingGuessess--;
-            gameMessageElement.textContent = `Fel fissning. Du har ${remainingGuessess} kvar.`;
+            gameMessageElement.innerText = `Fel fissning. Du har ${remainingGuessess} kvar.`;
    
         }
 
@@ -70,6 +71,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Funktion för att uppdatera 
     function updateGame() {
+        console.log("guessLetters", guessLetters);
+        console.log("selectedWord", selectedWord);
+
+        // Uppdatera ordet med rätt gissade bokstäver, annars _ 
+        let displayText = "";
+        for(let i = 0; i < selectedWord.length; i++) {
+            const letter = selectedWord[i];
+            console.log(letter);
+            if(guessLetters.includes(letter)===true) {
+                displayText += letter;
+            } else {
+                displayText += "_";
+            }
+        }
+
+        wordElement.innerText = displayText;
+
+        // Ska ge output som denna .. _ vid tom plats, bokstac vid fylld plats
+        //wordElement.textContent = "_ a _ a _ a"
 
     }
 
@@ -77,6 +97,26 @@ document.addEventListener("DOMContentLoaded", function() {
  
     // Funktion som kollar om man gissat rätt eller inte
     function checkGameStatus() {
+
+
+        // Om finalWord === selectedWord
+        let finalWord = wordElement.innerText;
+
+        if(finalWord === selectedWord) {
+            gameMessageElement.innerText = `Du har gissat rätt på alla bokstäver. Ordet var ${selectedWord}`;
+            // Win condition
+            gameActive = false;
+        } 
+          // Om remainGuesses är lika med 0
+        else if(remainingGuessess === 0) {
+             // Lose condition
+             gameActive = false;
+            gameMessageElement.innerText = `Dina försök är förbrukade. Du förlorada. Ordet var ${selectedWord}`;
+        } else {
+          console.log("du vann och förlorade inte heller");
+        }
+        
+ 
 
     }
   
